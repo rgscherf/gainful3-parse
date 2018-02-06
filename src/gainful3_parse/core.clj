bf8412bda214bbede8e628edb841ad4f43731e89
rgscherf <rgscherf@gmail.com>
2018-02-06 11:42:34 -0800

    high-level parse outline

--- src/gainful3_parse/core.clj
+++ src/gainful3_parse/core.clj
@@ -1,6 +1,24 @@
-(ns gainful3-parse.core)
+(ns gainful3-parse.core
+  (:require [gainful3-parse.orgs.ops]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn -main [& args]
+  ;; prune expired jobs from DB
+  (comment "???")
+
+  ;; get all current job URLs from DB, in a set
+  (def current-job-urls #{})
+
+  ;; make a coll of all scraping fns
+  (def scrape-fns [
+                   gainful3-parse.orgs.ops/execute
+                   ])
+
+  ;; apply each of the fns, with the current job URLS (current URLs are not re-parsed)
+  (->> scrape-fns
+       (map #(% current-job-urls))
+       flatten)
+
+  ;; insert all these new jobs into DB
+  (comment "???"))
+
