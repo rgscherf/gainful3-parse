1c6df9839d3ee3714ecc0303229cbff361d249ff
rgscherf <rgscherf@gmail.com>
2018-02-06 11:16:24 -0800

    add libs

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject gainful3-parse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+(defproject gainful3-parse "0.1.0"
+  :description "Web scraping for http://gainful.work"
+  :url "https://github.com/rgscherf/gainful3-parse"
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [enlive "1.1.1"]
+                 [etaoin "0.2.4"]
+                 ])
